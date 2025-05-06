2ff3dea365610be36aead5059c55e317400a5b40
KishoreKumar M B <kumar.kishore8091@gmail.com>
2025-05-06 09:23:16 +0530

    Add testcases

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+from .models import Task
 # Create your tests here.
+
+
+class TaskModelTest(TestCase):
+    def test_create_task(self):
+        task=Task.objects.create(
+            taskName="Sample Task",
+            dueDate=17400000,
+            isCompleted=False
+        )
+        self.assertEqual(task.taskName,"Sample Task")
+        self.assertFalse(task.isCompleted)
